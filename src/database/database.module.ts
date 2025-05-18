2d34058b34e1270c3d5484170eedc5ad1de0acb8
marcodavidd020 <marcodaviddtc@gmail.com>
2025-05-18 19:09:54 -0400

    feat(database, models): refactor and expand seeder modules
    - ✨ Consolidate seeder module imports in `DatabaseModule` through single `index` file
    - ✨ Expand `index.ts` to include exports for:
      - Category-related seeders
      - Product-related seeders
      - Cart-related seeders
      - Payment-related seeders
      - Coupon-related seeders
      - Order-related seeders
    - 🔄 Improve code organization by:
      - Removing redundant individual imports
      - Centralizing module exports

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersSeederModule } from './seeders/users/user.seeder.module';
-import { AddressesSeederModule } from './seeders/addresses/addresses.seeder.module';
-import { RolesSeederModule } from './seeders/roles/roles.seeder.module';
-import { PermissionsSeederModule } from './seeders/permissions/permissionsseeder.module';
-import { RolePermissionsSeederModule } from './seeders/roles/role-permissions.seeder.module';
-import { UserRolesSeederModule } from './seeders/users/user-roles.seeder.module';
+import {
+  UsersSeederModule,
+  AddressesSeederModule,
+  RolesSeederModule,
+  PermissionsSeederModule,
+  RolePermissionsSeederModule,
+  UserRolesSeederModule,
+  CategoriesSeederModule,
+  ProductsSeederModule,
+  CartsSeederModule,
+  PaymentsSeederModule,
+  CouponsSeederModule,
+  UserCouponsSeederModule,
+  OrdersSeederModule,
+  ReviewsSeederModule,
+} from './seeders/index';
 import { PostgresConfigModule } from '../config/database/postgres/config.module';
 import { PostgresConfigService } from '../config/database/postgres/config.service';
-import { CategoriesSeederModule } from './seeders/categories/categories.seeder.module';
-import { ProductsSeederModule } from './seeders/products/products.seeder.module';
-import { CartsSeederModule } from './seeders/carts/carts.seeder.module';
-import { PaymentsSeederModule } from './seeders/payments/payments.seeder.module';
-import { CouponsSeederModule } from './seeders/coupons/coupons.seeder.module';
-import { UserCouponsSeederModule } from './seeders/user-coupons/user-cupons.seeder.module';
-import { OrdersSeederModule } from './seeders/orders/orders.seeder.module';
-import { ReviewsSeederModule } from './seeders/reviews/reviews.seeder.module';
 
 /**
  * Módulo de base de datos
--- src/models/models.module.ts
+++ src/models/models.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { AddressesModule } from './addresses/addresses.module';
-import { RolesModule } from './roles/roles.module';
-import { PermissionsModule } from './permissions/permissions.module';
-import { CategoriesModule } from './categories/categories.module';
-import { ProductsModule } from './products/products.module';
-import { CartsModule } from './carts/carts.module';
-import { PaymentsModule } from './payments/payments.module';
-import { CouponsModule } from './coupons/coupons.module';
-import { UserCouponsModule } from './user-coupons/user-coupons.module';
-import { OrdersModule } from './orders/orders.module';
-import { ReviewsModule } from './reviews/reviews.module';
+import {
+  UsersModule,
+  AddressesModule,
+  RolesModule,
+  PermissionsModule,
+  CategoriesModule,
+  ProductsModule,
+  CartsModule,
+  PaymentsModule,
+  CouponsModule,
+  UserCouponsModule,
+  OrdersModule,
+  ReviewsModule,
+} from './index';
 
 @Module({
   imports: [
