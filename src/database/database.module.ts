917e125b4655647684204aa0887e59a12bc8d2a5
marcodavidd020 <marcodaviddtc@gmail.com>
2025-05-18 05:35:20 -0400

    feat(database): add orders seeder module and service
    - ✨ Import and register `OrdersSeederModule` in `DatabaseModule`
    - ✨ Integrate `OrdersSeederService` into seeding process to populate order data

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -14,6 +14,7 @@ import { CartsSeederModule } from './seeders/carts/seeder.module';
 import { PaymentsSeederModule } from './seeders/payments/seeder.module';
 import { CouponsSeederModule } from './seeders/coupons/seeder.module';
 import { UserCouponsSeederModule } from './seeders/user-coupons/seeder.module';
+import { OrdersSeederModule } from './seeders/orders/seeder.module';
 
 /**
  * Módulo de base de datos
@@ -52,6 +53,7 @@ import { UserCouponsSeederModule } from './seeders/user-coupons/seeder.module';
     PaymentsSeederModule,
     CouponsSeederModule,
     UserCouponsSeederModule,
+    OrdersSeederModule,
   ],
   exports: [
     UsersSeederModule,
@@ -66,6 +68,7 @@ import { UserCouponsSeederModule } from './seeders/user-coupons/seeder.module';
     PaymentsSeederModule,
     CouponsSeederModule,
     UserCouponsSeederModule,
+    OrdersSeederModule,
   ],
 })
 export class DatabaseModule {}
